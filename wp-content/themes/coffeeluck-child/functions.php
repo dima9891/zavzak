08f4fb8d342e8ae3f65618ac63ffb9ded315909e
dima9891 <rezvykh@mail.ru>
2022-09-17 20:35:23 +0300

    added styles for logo

--- wp-content/themes/coffeeluck-child/functions.php
+++ wp-content/themes/coffeeluck-child/functions.php
@@ -5,7 +5,11 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'header-style', get_stylesheet_directory() . '/css/header.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'my_plugin_add_stylesheet' );
+function my_plugin_add_stylesheet() {
+    wp_enqueue_style( 'head-style', get_stylesheet_directory_uri() . '/css/header.css');
 }
 
 // SVG upload enable hook
