35ca5402ad24811e10e951141df657595070e1ac
dima9891 <rezvykh@mail.ru>
2022-09-17 20:15:54 +0300

    minor fix

--- wp-content/themes/coffeeluck-child/functions.php
+++ wp-content/themes/coffeeluck-child/functions.php
@@ -5,7 +5,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'header-style', get_template_directory_uri() . '/css/header.css', nulls );
+    wp_enqueue_style( 'header-style', get_template_directory_uri() . '/css/header.css', null );
 }
 
 // SVG upload enable hook
